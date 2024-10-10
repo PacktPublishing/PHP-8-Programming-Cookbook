2e5e0d2a0380ebb0803cdae0915bc76b9137981e
fred <doug@unlikelysource.com>
2024-10-10 17:18:01 +0700

    2024-10-10 DB: Updated development image

--- docker/secrets.sh
+++ docker/secrets.sh
@@ -9,6 +9,6 @@ export REPO_DIR=/repo
 export REPO_BACKUP_DIR=/repo/backups
 export DOC_ROOT=/repo/public
 export CONTAINER_NAME=php8cookbook
-export CONTAINER_IP=10.10.10.10
+export CONTAINER_IP=10.10.10.88
 export CONTAINER_SUBNET=10.10.10.0\/24
 export DB_ADMIN_VER=4.8.1
--- src/Chapter02/ch02_promoted_notices_warnings_errors.php
+++ src/Chapter02/ch02_promoted_notices_warnings_errors.php
@@ -24,19 +24,17 @@ A number of notices have been converted into warnings:
     Attempting to write to an array index of a scalar value.
     Attempting to unpack a non-array/Traversable.
     Attempting to access unqualified constants which are undefined. Previously, unqualified constant accesses resulted in a warning and were interpreted as strings.
-    Passing the wrong number of arguments to a non-variadic built-in function will throw an ArgumentCountError.
-    Passing invalid countable types to count() will throw a TypeError.
+            'Passing the wrong number of arguments to a non-variadic built-in function will throw an ArgumentCountError.
+            'Passing invalid countable types to count() will throw a TypeError.
 
 */
-
-$obj = new class() {};
 
 $arr = [
     'Notice to Warning' => [
         'Attempting to read an undefined variable' => 
             function () { echo $a; },
         'Attempting to read an undefined property' => 
-            function () { echo (new class() {})->undefined; },
+            function () { $obj = new class() {}; echo $obj->undefined; },
         'Attempting to read an undefined array key' => 
             function () { $arr = ['A' => 111]; echo $arr['B']; },
         'Attempting to read a property of a non-object' => 
@@ -44,17 +42,50 @@ $arr = [
         'Attempting to access an array index of a non-array' => 
             function () { $a = 12345; echo $a[0]; },
         'Attempting to convert an array to string' => 
-            function () { $arr = ['A' => 111]; var_dump((string) $arr); },
+            function () { $arr = ['A' => 111]; echo (string) $arr; },
         'Attempting to use a resource as an array key' => 
-            function () { $fh = fopen('gettysburg_address.txt', 'r'); $arr[$fh] = 'Gettysburg'; },
-        'Attempting to use NULL, a boolean, or a float as a string offset' => \
+            function () { $fh = fopen('gettysburg_address.txt', 'r'); 
+                          $arr[$fh] = 'Gettysburg'; 
+                          var_dump($arr);
+            },
+        'Attempting to use NULL, a boolean, or a float as a string offset' =>
             function () { $str = 'ABC'; echo $str[TRUE]; },
         'Attempting to read an out-of-bounds string offset' => 
             function () { $str = 'ABC'; echo $str[99]; },
         'Attempting to assign an empty string to a string offset' => 
-            function () { $str = 'ABC'; $str[0] = ''; },
+            function () { $str = 'ABC'; $str[0] = ''; echo $str; },
     ],
     'Warnings to Error' => [
+        'Attempting to write to a property of a non-object' 
+            => function () { $a = 123; $a->test = 'TEST'; var_dump($a); },
+        'Attempting to append an element to an array for which the PHP_INT_MAX key is already used' 
+            => function () { $a[PHP_INT_MAX] = 888; $a[] = 999; var_dump($a); },
+        'Attempting to use an invalid type (array or object) as an array key or string offset' 
+            => function () { $obj = new class () {}; $a[$obj] = 'TEST';  var_dump($a); },
+        'Attempting to write to an array index of a scalar value' 
+            => function () { $str = 'ABC'; $str[0] = 'Z'; echo $str; },
+        'Attempting to unpack a non-array/Traversable' 
+            => function () { $it = new ArrayIterator([1,2,3]); $a = [...$it]; var_dump($it); },
+        'Attempting to access unqualified constants which are undefined' 
+            => function () { echo NOT_DEFINED; },
+        'Passing the wrong number of arguments to a non-variadic built-in function will throw an ArgumentCountError' 
+            => function () { $a = 'TEST'; echo strlen($a, 'TEST'); },
+        'Passing invalid countable types to count() will throw a TypeError' 
+            => function () { $a = 123; echo count($a); },
     ],
 ];
-
+$bar = str_repeat('-', 80) . PHP_EOL;
+foreach ($arr as $type => $list) {
+    echo $bar . strtoupper($type) . ':' . PHP_EOL . $bar;
+    foreach ($list as $item => $callback) {
+        try {
+            echo $item . PHP_EOL . $bar;
+            $callback();
+        } catch (Exception $e) {
+            echo 'PHP Exception: ' . get_class($e) . ': ' . $e->getMessage();
+        } catch (Error $e) {
+            echo 'PHP Fatal Error: ' . get_class($e) . ': ' . $e->getMessage();
+        }
+        echo PHP_EOL;
+    }
+}
