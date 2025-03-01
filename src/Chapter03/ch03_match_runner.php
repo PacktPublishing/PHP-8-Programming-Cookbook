78735cfa2f4747257f259c917bf9645e09553682
dbierer <doug@unlikelysource.com>
2025-03-01 14:01:30 +0700

    2025-03-01: Adding example to chapter 3

---
+++ src/Chapter03/ch03_match_runner.php
@@ -0,0 +1,24 @@
+<?php
+// decision tree to render using match() {}
+/*
+Gender	Male	Item	Clothing	Shirts	MensShirts.html
+				Pants	MensPants.html
+				Jackets	MensJackets.html
+				Shorts	MensShorts.html
+			Shoes	Sports	MensShoesSports.html
+				Dress	MensShoesDress.html
+				Casual	MensShoesCasual.html
+			Accessories	Belts	MensAccessBelts.html
+				Wallets	MensAccessWallets.html
+	Female	Item	Clothing	Skirts	WomensSkirts.html
+				Blouses	WomensBlouses.html
+				Dresses	WomensDresses.html
+				Pants	WomensPants.html
+				Shorts	WomensShorts.html
+			Shoes	Sports	WomensShoesSports.html
+				Dress	WomensShoesDress.html
+				Casual	WomensShoesCasual.html
+			Accessories	Belts	WomensAccessBelts.html
+				Purses	WomensAccessPurses.html
+				Jewelry	WomensAccessJewelry.html
+*/
