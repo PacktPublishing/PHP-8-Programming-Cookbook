1512ec436b9d8b2d5861f7c3f28728f1b01c9728
dbierer <doug@unlikelysource.com>
2025-03-09 15:59:25 +0700

    2025-03-09: Added code examples for Chapter 3

---
+++ src/Chapter03/ch03_match_func_lib.php
@@ -0,0 +1,50 @@
+<?php
+namespace Library;
+// define supported mime types
+define('ACCEPT_X', 'application/xml');
+define('ACCEPT_J', 'application/json');
+define('ACCEPT_H', 'text/html');
+define('ACCEPT_T', 'text/plain');
+// defines functions used in ch03_match_runner.php
+// XML
+$x = function ($data) {
+    header('Content-Type: ' . ACCEPT_X);
+    $xml = xmlwriter_open_memory();
+    $xml->startDocument();
+    $xml->startElement('Item');
+    foreach ($data as $key => $value) {
+        $xml->startElement('Key');
+        $xml->text((string) $key);
+        $xml->endElement();
+        $xml->startElement('Value');
+        $xml->text((string) $value);
+        $xml->endElement();
+    }
+    $xml->endElement(); // Item
+    return $xml->outputMemory();
+};
+// JSON
+$j = function ($data) {
+    header('Content-Type: ' . ACCEPT_J);
+    return json_encode($data, JSON_PRETTY_PRINT); 
+};
+// HTML
+$h = function ($data) {
+    header('Content-Type: ' . ACCEPT_H);
+    $output = '<table border=1>' . PHP_EOL;
+    foreach ($data as $key => $value) {
+        $output .= sprintf("  <tr><th>%s</th><td>%s</td></tr>\n",
+                           (string) $key, (string) $value);
+    }
+    $output .= '</table>' . PHP_EOL;
+    return $output;
+};
+// Text
+$t = function ($data) {
+    header('Content-Type: ' . ACCEPT_T);
+    $output = '';
+    foreach ($data as $key => $value) {
+        $output .= $key . ":\t" . $value . PHP_EOL;
+    }
+    return $output;
+};
--- src/Chapter03/ch03_match_runner.php
+++ src/Chapter03/ch03_match_runner.php
@@ -1,24 +1,50 @@
 <?php
-// decision tree to render using match() {}
-/*
-Gender	Male	Item	Clothing	Shirts	MensShirts.html
-				Pants	MensPants.html
-				Jackets	MensJackets.html
-				Shorts	MensShorts.html
-			Shoes	Sports	MensShoesSports.html
-				Dress	MensShoesDress.html
-				Casual	MensShoesCasual.html
-			Accessories	Belts	MensAccessBelts.html
-				Wallets	MensAccessWallets.html
-	Female	Item	Clothing	Skirts	WomensSkirts.html
-				Blouses	WomensBlouses.html
-				Dresses	WomensDresses.html
-				Pants	WomensPants.html
-				Shorts	WomensShorts.html
-			Shoes	Sports	WomensShoesSports.html
-				Dress	WomensShoesDress.html
-				Casual	WomensShoesCasual.html
-			Accessories	Belts	WomensAccessBelts.html
-				Purses	WomensAccessPurses.html
-				Jewelry	WomensAccessJewelry.html
-*/
+namespace Library;
+include __DIR__ . '/ch03_match_func_lib.php';       // loads functions
+if (!empty($_POST)) {
+    // load functions
+    // determine format
+    $accept = strtolower(trim(strip_tags($_POST['accept'] ?? $_SERVER['HTTP_ACCEPT'] ?? ACCEPT_T)));
+    // sanitize data
+    $data = array_map(function ($val) { return trim(strip_tags($val)); }, ($_POST ?? []));
+    // callback tree
+    echo match (TRUE) {
+        str_contains($accept, ACCEPT_X) => $x($data),
+        str_contains($accept, ACCEPT_J) => $j($data),
+        str_contains($accept, ACCEPT_H) => $h($data),
+        str_contains($accept, ACCEPT_T) => $t($data),
+        default => 'No post data'
+    };
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Chapter 3 -- Match</title>
+<meta name="generator" content="Geany 2.0" />
+</head>
+<body>
+Enter the following:
+<form method="post" action="/src/Chapter03/ch03_match_runner.php">
+<table>
+<tr><th>First Name</th><td><input type="text" name="first" /></td></tr>
+<tr><th>Last Name</th><td><input type="text" name="last" /></td></tr>
+<tr><th>Email Address</th><td><input type="text" name="email" /></td></tr>
+<tr>
+    <th>Output Format:</th>
+    <td>
+        <select name="accept">
+            <option value="<?= ACCEPT_X ?>">XML</option>
+            <option value="<?= ACCEPT_J ?>">JSON</option>
+            <option value="<?= ACCEPT_H ?>">HTML</option>
+            <option value="<?= ACCEPT_T ?>">Text</option>
+        </select>
+    </td>
+</tr>
+<tr><th>&nbsp;</th><td><input type="submit" /></td></tr>
+</table>
+</form>
+</body>
+</html>
