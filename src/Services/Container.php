2fdfb516a803cc3e21d6191154ff0d684423ca5d
dbierer <doug@unlikelysource.com>
2025-05-25 13:58:57 +0700

    2025-05-25: Working on code for Chap 4

--- src/Services/Container.php
+++ src/Services/Container.php
@@ -6,36 +6,42 @@ use Psr\Container\ContainerInterface;
 class Container implements ContainerInterface
 {
     public ArrayObject $storage;
-    protected $instance = NULL;
+    protected static $instance = NULL;
     private function __construct()
     {
         $this->storage = new ArrayObject();
     }
     public static function getInstance() : static
     {
-        if (empty($this->instance)) {
-            $this->instance = new static();
+        if (empty(static::$instance)) {
+            static::$instance = new static();
         }
-        return $this->instance;
+        return static::$instance;
     }
     public function get(string $key) : mixed
     {
-        return $this->instance->storage[$key] ?? NULL;
+        // TODO: add provision to retrieve service from cache
+        return static::$instance->storage[$key] ?? NULL;
     }
     public function has(string $key) : bool
     {
         return !empty($this?->instance?->storage[$key]);
     }
-    public function add(string $key, mixed $service) : bool
+    public function add(string $key, callable $service) : bool
     {
-        $this->instance->storage[$key] = $service;
-        return $this->instance->has($key);
+        // TODO: store service as LazyGhost
+        static::$instance->storage[$key] = $service;
+        return static::$instance->has($key);
     }
+    public function cache(string $key) : bool
+    {
+        // TODO: serialize and cache the service
+    }    
     public function remove(string $key) : bool
     {
         $ok = FALSE;
         if ($this->has($key)) {
-            unset($this->instance->storage[$key]);
+            unset(static::$instance->storage[$key]);
             $ok = TRUE;
         }
         return $ok;
